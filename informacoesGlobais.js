a70f4bb61e2e28bfac4ab355fa6e74a0fd4b3542
Estudante Alura- GIULIA <107186936+giuliiaas15@users.noreply.github.com>
2024-10-01 22:00:20 -0300

    01de outubro - prox. aula continuar modulo 5 aula 4

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -2,14 +2,14 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function vizualizarInformacoesGlobais() {
   const res = await fetch(url)
-  const dados await res.json()
-  const paragrafo document.createElement('p')
+  const dados = await res.json()
+  cosole.log(dados);
+  const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
- paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-  console.log(paragrafo)
-
-  const container = document.getElementById('graficos-container')
-container.appendChild(paragrafo)
+  paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+  
+  const container = document.getElementById(‘graficos-container’)
+  container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais()
