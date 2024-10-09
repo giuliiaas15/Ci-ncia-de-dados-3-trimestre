f65d9420430bb296d486919ff7cda295b6e2fc73
Estudante Alura- GIULIA <107186936+giuliiaas15@users.noreply.github.com>
2024-10-08 22:01:26 -0300

    int

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,7 +4,11 @@ async function visualizarInformacoesGlobais() {
 
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados.tempo_medio);
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    
 }
 
     visualizarInformacoesGlobais()
